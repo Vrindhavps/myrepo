44b13f031dd9c9edf2377a8f88ecfd339f08ac30
vrindha <vrindha@LAPTOP-JA1AAE89>
2023-07-25 10:16:41 +0530

    my ninth message

--- MavenProj2/src/main/java/hellowWorld2/multi4.java
+++ MavenProj2/src/main/java/hellowWorld2/multi4.java
@@ -9,7 +9,7 @@ public class multi4 extends multi3{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-multi4 sc=new multi4();
+multi4 sc=new multi4();//
 sc.show();
 sc.display();
 sc.show2();
